f7c78643f79142895bec79121c95992f5061c131
montura <fatum.est.series.causarum@gmail.com>
2021-05-10 14:20:20 +0300

    Working on personality function

--- src/cpp_abi/cpp_abi.cpp
+++ src/cpp_abi/cpp_abi.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <typeinfo>
+#include <unwind.h>
 
 namespace __cxxabiv1 {
   struct __class_type_info {
@@ -20,6 +21,28 @@ namespace std {
 char exception_buff[EXCEPTION_BUFF_SIZE];
 
 extern "C" {
+  typedef void (*unexpected_handler)(void);
+  typedef void (*terminate_handler)(void);
+
+  struct __cxa_exception {
+    std::type_info* exceptionType;
+
+    void (* exceptionDestructor)(void*);
+
+    unexpected_handler unexpectedHandler;
+    terminate_handler terminateHandler;
+    __cxa_exception* nextException;
+
+    int handlerCount;
+    int handlerSwitchValue;
+    const char* actionRecord;
+    const char* languageSpecificData;
+    void* catchTemp;
+    void* adjustedPtr;
+
+    _Unwind_Exception unwindHeader;
+  };
+
   void* __cxa_allocate_exception(size_t thrown_size) {
     printf("alloc ex %lu\n", thrown_size);
     if (thrown_size > EXCEPTION_BUFF_SIZE) {
@@ -36,13 +59,26 @@ extern "C" {
   {
     printf("__cxa_throw called\n");
 
+    __cxa_exception *header = ((__cxa_exception *) thrown_exception - 1);
+    _Unwind_RaiseException(&header->unwindHeader);
+
     // __cxa_throw never returns
     printf("no one handled __cxa_throw, terminate!\n");
     exit(0);
   }
 
-  void __gxx_personality_v0() {
-    printf("Personality function\n");
+  _Unwind_Reason_Code __gxx_personality_v0 (int version,
+                                            _Unwind_Action actions,
+                                            uint64_t exceptionClass,
+                                            _Unwind_Exception* unwind_exception,
+                                            _Unwind_Context* context)
+  {
+    printf("Personality function begin!\n");
+    printf("\tversiov = %d, action = %d \n", version, actions);
+    printf("\texceptionClass = %p\n", (void *) exceptionClass);
+    printf("\tunwind_exception = %p\n", unwind_exception);
+    printf("\tcontext = %p\n", context);
+    printf("Personality function end!\n");
   }
 
   void __cxa_begin_catch() {
