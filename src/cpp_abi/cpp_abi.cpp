8064021067ac89f5ab976e3f09f1ca1aca76f601
montura <fatum.est.series.causarum@gmail.com>
2021-05-15 21:32:03 +0300

    Parse language data specific area to get type_info table in order to process an exception in the proper catch-block

--- src/cpp_abi/cpp_abi.cpp
+++ src/cpp_abi/cpp_abi.cpp
@@ -1,4 +1,3 @@
-
 #include <cstdio>
 #include <cstdlib>
 #include <typeinfo>
@@ -34,24 +33,6 @@ extern "C" {
 
   void __cxa_free_exception(void* thrown_exception);
 
-  LSDA_ptr read_lsda_header(_Unwind_Context* const context) {
-    // Get the beginning of the raw LSDA
-    auto lsda = reinterpret_cast<LSDA_ptr>(_Unwind_GetLanguageSpecificData(context));
-    printf("\tLSDA = %p\n", lsda);
-
-    if (lsda == 0) {
-      return 0;
-    }
-
-    // Read LSDA header for the LSDA
-    LSDA_Header header(&lsda);
-    printf("\tLSDA header = %p\n", &header);
-    printf("\t\tLSDA header->lsda_start_encoding = %u\n", header.start_encoding);
-    printf("\t\tLSDA header->lsda_type_encoding = %u\n", header.type_encoding);
-    printf("\t\tLSDA header->lsda_call_site_table_length = %u\n", header.call_site_table_length);
-    return lsda;
-  }
-
   uintptr_t getIP(_Unwind_Context* const context) {
     // Calculate what IP was just before the exception was thrown for this stack frame
     //  1. The processor always increments the IP when it executes an instruction. Throwing exception is not an exception.
@@ -63,12 +44,36 @@ extern "C" {
     return ip;
   }
 
-  bool IP_is_valid(uintptr_t throw_ip, uintptr_t curr_stack_frame, LSDA_Call_Site const& cs) {
+  void setIP(_Unwind_Context* const context, _Unwind_Exception* unwind_exception, uintptr_t func_start, uint8_t cs_length) {
+    int r0 = __builtin_eh_return_data_regno(0);
+    int r1 = __builtin_eh_return_data_regno(1);
+
+    // Set data registers for exception handling (hardcodes the exception type; we'll fix that later on)
+
+    // See http://www.ucw.cz/~hubicka/papers/abi/node25.html#SECTION00925000000000000000
+    // In that case, only registers %rdi, %rsi, %rdx, %rcx should be used.
+    // These scratch registers are reserved for passing arguments between the personality routine and the landing pads.
+
+    // See 1.6.3 Transferring Control to a Landing Pad (https://itanium-cxx-abi.github.io/cxx-abi/abi-eh.html#base-personality)
+    //  - The landing pad may receive arguments from the runtime,
+    //    typically passed in registers set using _Unwind_SetGR by the personality routine.
+    //    For a landing pad that can call to _Unwind_Resume, one argument must be the exceptionObject pointer,
+    //    which must be preserved to be passed to _Unwind_Resume.
+    //  - The landing pad may receive other arguments, for instance a switch value indicating the type of the exception.
+
+    _Unwind_SetGR(context, r0, (uintptr_t)(unwind_exception));
+    _Unwind_SetGR(context, r1, (uintptr_t)(1));
+
+    /* Setup the address we should jump at to reach the code where there is the "something" we found.  */
+    _Unwind_SetIP(context, func_start + cs_length);
+  }
+
+  bool IP_is_valid(uintptr_t throw_ip, uintptr_t curr_stack_frame, LSDA_Call_Site const* cs) {
     // Calculate the range of the IP range valid for this landing pad;
     // - If this LP can handle the current exception then the IP for this stack frame must be in this range
 
-    uintptr_t try_start = curr_stack_frame + cs.start;
-    uintptr_t try_end = curr_stack_frame + cs.start + cs.len;
+    uintptr_t try_start = curr_stack_frame + cs->start;
+    uintptr_t try_end = curr_stack_frame + cs->start + cs->len;
 
     bool is_valid = (throw_ip >= try_start) && (throw_ip <= try_end);
 
@@ -77,65 +82,82 @@ extern "C" {
     return is_valid;
   }
   
-  void process_lsda_cs_table(LSDA_ptr lsda, _Unwind_Context* const context, _Unwind_Exception* unwind_exception) {
-    // Read the LSDA CS header
-    LSDA_Call_Site_Header cs_header(&lsda);
-    printf("\tLSDA cs_header = %p\n", &cs_header);
+  _Unwind_Reason_Code parse_LSDA(_Unwind_Context* const context, _Unwind_Action actions,
+                                 _Unwind_Exception* unwind_exception)
+  {
+    // Get the beginning of the raw LSDA
+    auto lsda_ptr = reinterpret_cast<LSDA_ptr>(_Unwind_GetLanguageSpecificData(context));
+    if (!lsda_ptr) {
+      return _URC_CONTINUE_UNWIND;
+    }
 
-    // Calculate where the end of the LSDA CS table is
-    const LSDA_ptr lsda_cs_table_end = lsda + cs_header.length;
+    struct LSDA lsda(lsda_ptr);
 
     // Loop through each entry in the CS table
-    while (lsda < lsda_cs_table_end) {
-      LSDA_Call_Site cs(&lsda);
-      printf("\tFound a CS:\n");
-      printf("\t\tcs_start: %i\n", cs.start);
-      printf("\t\tcs_len: %i\n", cs.len);
-      printf("\t\tcs_lp: %i\n", cs.lp);
-      printf("\t\tcs_action: %i\n", cs.action);
+    LSDA_Call_Site_Table& cs_table = lsda.cs_table;
+    const LSDA_Call_Site* cs;
 
-      // Landing pad offset is valid
-      if (cs.lp) {
-        uintptr_t throw_ip = getIP(context);
-        uintptr_t func_start = _Unwind_GetRegionStart(context); // points to the current stack frame (function)
+    // Landing pad offset is valid
+    uintptr_t throw_ip = getIP(context);
+    uintptr_t func_start = _Unwind_GetRegionStart(context); // points to the current stack frame (function)
 
-        if (!IP_is_valid(throw_ip, func_start, cs)) {
-          continue;
-        }
-
-        int r0 = __builtin_eh_return_data_regno(0);
-        int r1 = __builtin_eh_return_data_regno(1);
-
-        // Set data registers for exception handling (hardcodes the exception type; we'll fix that later on)
-
-        // See http://www.ucw.cz/~hubicka/papers/abi/node25.html#SECTION00925000000000000000
-        // In that case, only registers %rdi, %rsi, %rdx, %rcx should be used.
-        // These scratch registers are reserved for passing arguments between the personality routine and the landing pads.
-
-        // See 1.6.3 Transferring Control to a Landing Pad (https://itanium-cxx-abi.github.io/cxx-abi/abi-eh.html#base-personality)
-        //  - The landing pad may receive arguments from the runtime,
-        //    typically passed in registers set using _Unwind_SetGR by the personality routine.
-        //    For a landing pad that can call to _Unwind_Resume, one argument must be the exceptionObject pointer,
-        //    which must be preserved to be passed to _Unwind_Resume.
-        //  - The landing pad may receive other arguments, for instance a switch value indicating the type of the exception.
-
-        _Unwind_SetGR(context, r0, (uintptr_t)(unwind_exception));
-        _Unwind_SetGR(context, r1, (uintptr_t)(1));
-
-        /* Setup the address we should jump at to reach the code where there is the "something" we found.  */
-        _Unwind_SetIP(context, func_start + cs.lp);
-        break;
+    while ((cs = cs_table.next_call_site_entry())) {
+      if (not cs->lp || not IP_is_valid(throw_ip, func_start, cs)) {
+        continue;
       }
+      // We found a landing pad for this exception; resume execution
+      // Get the offset into the action table for this LP
+      if (cs->action > 0) {
+        // cs->action is the offset + 1; that way cs->action == 0
+        // means there is no associated entry in the action table
+        const size_t action_offset = cs->action - 1;
+        const LSDA_ptr action = lsda.action_table_start + action_offset;
+
+        // For a landing pad with a catch the action table will
+        // hold an index to a list of types
+        int type_index = action[0];
+
+        // todo: fix Segmentation fault! Wrong address for type_ifo!!!!
+        // Get the type of the exception we can handle
+        const void* catch_type_info = lsda.types_table_start[ -1 * type_index ];
+        const std::type_info *catch_ti = (const std::type_info *) catch_type_info;
+
+        // Get the type of the original exception being thrown
+//        __cxa_exception* exception_header = (__cxa_exception*)(unwind_exception+1) - 1;
+//        std::type_info *org_ex_type = exception_header->exceptionType;
+//
+//        printf("%s thrown, catch handles %s\n",
+//               org_ex_type->name(),
+//               catch_ti->name());
+
+        // Check if the exception being thrown is of the same type
+        // than the exception we can handle
+//        if (org_ex_type->name() != catch_ti->name())
+//          continue;
+      }
+
+      // If we are on search phase, tell _Unwind_ we can handle this one
+      if (actions & _UA_SEARCH_PHASE) {
+        return _URC_HANDLER_FOUND;
+      }
+
+      setIP(context, unwind_exception, func_start, cs->lp);
+      return _URC_INSTALL_CONTEXT;
     }
+    return _URC_CONTINUE_UNWIND;
   }
 
   void __cxa_throw(void* thrown_exception,
-                   struct std::type_info* tinfo,
+                   std::type_info *tinfo,
                    void (* dest)(void*))
   {
-    printf("__cxa_throw called\n");
+    printf("__cxa_throw called, thrown_exception = %p\n", thrown_exception);
 
-    __cxa_exception *header = (reinterpret_cast<__cxa_exception *>(thrown_exception) - 1);
+    __cxa_exception *header = ((__cxa_exception *) thrown_exception + 1) - 1;
+
+    // We need to save the type info in the exception header _Unwind_ will
+    // receive, otherwise we won't be able to know it when unwinding
+    header->exceptionType = tinfo;
     _Unwind_RaiseException(&header->unwindHeader);
 
     // __cxa_throw never returns
@@ -161,12 +183,7 @@ extern "C" {
       res = _URC_HANDLER_FOUND;
     } else if (actions & _UA_CLEANUP_PHASE) {
       printf("\tphase: _UA_CLEANUP_PHASE\n");
-      LSDA_ptr lsda = read_lsda_header(context);
-      if (!lsda) {
-        return _URC_CONTINUE_UNWIND;
-      }
-      process_lsda_cs_table(lsda, context, unwind_exception);
-      res = _URC_INSTALL_CONTEXT;
+      res = parse_LSDA(context, actions, unwind_exception);
     } else {
       printf("\tPersonality function, error\n");
       res = _URC_FATAL_PHASE1_ERROR;
--- src/cpp_abi/lsda.h
+++ src/cpp_abi/lsda.h
@@ -9,27 +9,35 @@ namespace LSDA {
  * &LSDA_ptr will be a non-const pointer to a const place in memory
  */
   typedef const uint8_t* LSDA_ptr;
+  typedef const void* LSDA_type_info;
 
   struct LSDA_Header {
     /**
      * Read the LSDA table into a struct; advances the lsda pointer
      * as many bytes as read
      */
-    explicit LSDA_Header(LSDA_ptr *lsda) {
-      LSDA_ptr read_ptr = *lsda;
+    explicit LSDA_Header(LSDA_ptr* lsda) {
+      LSDA_ptr header = *lsda;
 
       // Copy the LSDA fields
-      start_encoding = read_ptr[0];
-      type_encoding = read_ptr[1];
-      call_site_table_length = read_ptr[2];
+      start_encoding = header[0];
+      type_encoding = header[1];
+      type_table_offset = header[2];
+
+      printf("\tLSDA header = %p\n", &header);
+      printf("\t\theader->start_encoding = %u\n", start_encoding);
+      printf("\t\theader->type_encoding = %u\n", type_encoding);
+      printf("\t\theader->call_site_table_length = %u\n", type_table_offset);
 
       // Advance the lsda pointer
-      *lsda = read_ptr + sizeof(LSDA_Header);
+      *lsda = header + sizeof(LSDA_Header);
     }
 
     uint8_t start_encoding;
     uint8_t type_encoding;
-    uint8_t call_site_table_length;
+
+    // This is the offset, from the end of the header, to the types table
+    uint8_t type_table_offset;
   };
 
   struct LSDA_Call_Site_Header {
@@ -40,6 +48,10 @@ namespace LSDA {
       encoding = read_ptr[0];
       length = read_ptr[1];
 
+      printf("\tLSDA Call Site header = %p\n", read_ptr);
+      printf("\t\theader->encoding = %u\n", encoding);
+      printf("\t\theader->length = %u\n", length);
+
       // Advance the lsda pointer
       *lsda = read_ptr + sizeof(LSDA_Call_Site_Header);
     }
@@ -49,17 +61,25 @@ namespace LSDA {
   };
 
   struct LSDA_Call_Site {
-    explicit LSDA_Call_Site(LSDA_ptr *lsda) {
-      LSDA_ptr read_ptr = *lsda;
+    explicit LSDA_Call_Site(LSDA_ptr * lsda) {
+      if (lsda) {
+        LSDA_ptr cs_entry = *lsda;
 
-      // Copy the LSDA call site fields
-      start = read_ptr[0];
-      len = read_ptr[1];
-      lp = read_ptr[2];
-      action = read_ptr[3];
+        // Copy the LSDA call site fields
+        start = cs_entry[0];
+        len = cs_entry[1];
+        lp = cs_entry[2];
+        action = cs_entry[3];
 
-      // Advance the lsda pointer
-      *lsda = read_ptr + sizeof(LSDA_Call_Site);
+        printf("\tFound a CS:\n");
+        printf("\t\tcs_start: %i\n", start);
+        printf("\t\tcs_len: %i\n", len);
+        printf("\t\tcs_lp: %i\n", lp);
+        printf("\t\tcs_action: %i\n", action);
+
+        // Advance the lsda pointer
+        *lsda = cs_entry + sizeof(LSDA_Call_Site);
+      }
     }
     // Note start, len and lp would be void*'s, but they are actually relative addresses:
     //  - start and lp are relative to the start of the function
@@ -69,5 +89,69 @@ namespace LSDA {
     uint8_t len;     // the length of the block that might throw
     uint8_t lp;      // landing pad offset
     uint8_t action;  // the offset in the action table + 1 (0 means no action). Used to run destructors
+  };
+
+  struct LSDA_Call_Site_Table {
+    // A pointer to the start of the call site table
+    LSDA_ptr const cs_table_start;
+
+    // A pointer to the end of the call site table
+    LSDA_ptr const cs_table_end;
+
+    explicit LSDA_Call_Site_Table(LSDA_ptr raw_lsda, uintptr_t call_site_table_length) :
+      cs_table_start(raw_lsda),
+      cs_table_end(raw_lsda + call_site_table_length)
+    {
+      cs_entry_ptr = cs_table_start;
+    }
+
+    const LSDA_Call_Site* next_call_site_entry() {
+      if (cs_entry_ptr >= cs_table_end) {
+        return nullptr;
+      }
+
+      // Copy the call site table and advance the cursor by sizeof(LSDA_Call_Site).
+      // We need to copy the struct here because there might be alignment issues otherwise
+      cs_entry = LSDA_Call_Site(&cs_entry_ptr);
+
+      return &cs_entry;
+    };
+
+  private:
+    LSDA_Call_Site cs_entry {nullptr };
+    LSDA_ptr cs_entry_ptr;
+  };
+
+  struct LSDA {
+    LSDA_Header const header;
+
+    // The types_table_start holds all the types this stack frame could handle
+    // - This table will hold pointers to struct type_info so this is actually a pointer to a list of pointers
+    LSDA_type_info* const types_table_start;
+
+    // With the call site header we can calculate the length of the call site table
+    LSDA_Call_Site_Header const cs_header;
+
+    LSDA_Call_Site_Table cs_table;
+
+    // A pointer to the start of the action table, where an action is
+    // defined for each call site
+    LSDA_ptr const action_table_start;
+
+    explicit LSDA(LSDA_ptr raw_lsda) :
+      // Read LSDA header, advance the ptr
+      header(&raw_lsda),
+      // Read Type Info Table
+      //  !!! Actually this ptr points to the end of the table,
+      //  but since the action index will hold a negative index for this table we assume it's the beginning
+      types_table_start((const void**)(raw_lsda + header.type_table_offset)),
+      // Read LSDA call site header, advance the ptr
+      cs_header(&raw_lsda),
+      // Read the call site table, it starts immediately after the Call Site header
+      cs_table(raw_lsda, cs_header.length),
+      // Read the action table, it starts immediately after the Call Site Table
+      action_table_start(cs_table.cs_table_end)
+    {}
+
   };
 }
